cac660184ef35ee0d62657d4a42ccca57079b3d7
Néstor Torres <nestor.f.torres@gmail.com>
2020-06-28 03:19:17 -0500

    Data Structure Preparation

--- Scripts/collection/data_collection.R
+++ Scripts/collection/data_collection.R
@@ -2,13 +2,15 @@
 ########                            Calidad del Aire Monterrey                  ########
 ########                        Autor Néstor Felipe Torres Pacheco              ########
 ########################################################################################
-
 # Instalación de Librería JSON
 install.packages("jsonlite", repos="http://cran.r-project.org")
 require(jsonlite)
 # Instalación de Librería httr
 install.packages("httr")
 require(httr)
+# Instalación de Librería Tidyverse
+install.packages("tidyverse")
+library(tidyverse)
 #Estructura API
 base <- "https://api.datos.gob.mx/v2/"
 endpoint <- "sinaica"
@@ -35,3 +37,30 @@ for(i in 2:pages){
 }
 #Exportar data a csv
 write.csv(air_quality_mty_df, "/home/nestor/Documentos/Projects/Maestría/CalidadAireMTY/data/data.csv", row.names = TRUE)
+#Estaciones de Monitoreo
+endpoint_est <- "sinaica-estaciones"
+id<-72
+callAPI_EST <- paste(base,endpoint_est,"?","redesid","=", id, sep="")
+get_est <- GET(callAPI_EST)
+data_est <- content(get_est,"text")
+estaciones <- fromJSON(data_est, flatten=TRUE)
+estaciones_df <- as.data.frame(estaciones)
+#Eliminación de Columnas innecesarias
+estaciones_df$pagination.pageSize <- NULL
+estaciones_df$pagination.page <- NULL
+estaciones_df$pagination.total <- NULL
+estaciones_df$X1 <- NULL
+#Renombra Columnas
+names(estaciones_df)[1] <- "hashCode"
+names(estaciones_df)[2] <- "latitude"
+names(estaciones_df)[3] <- "longitude"
+names(estaciones_df)[4] <- "stationId" 
+names(estaciones_df)[5] <- "stationName"
+names(estaciones_df)[6] <- "code"
+names(estaciones_df)[7] <- "netId"
+#Escribir datos en archivo .csv
+write.csv(estaciones_df, "/home/nestor/Documentos/Projects/Maestría/CalidadAireMTY/data/stations.csv", row.names = TRUE)
+
+
+
+head(estaciones_df)
